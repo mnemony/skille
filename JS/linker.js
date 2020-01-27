0fbecc4ff59cbc6ff6d024feeb08d9fd77a55faa
Maksym <maxymprz@gmail.com>
2020-01-27 21:34:03 +0100

    about

---
+++ JS/linker.js
@@ -0,0 +1,8 @@
+function myGit() {
+    window.open("https://github.com/mnemony", "_blank");
+};
+
+
+function myLin() {
+    window.open("https://www.linkedin.com/in/maksym-przewo%C5%BAny-b550b6156/", "_blank");
+};
--- JS/loader.js
+++ JS/loader.js
@@ -2,5 +2,5 @@ window.addEventListener("load", function () {
     const loader = document.querySelector(".loader");
     this.setTimeout(function () {
         loader.className += " hidden"
-    }, 1000)
+    }, 750)
 });
