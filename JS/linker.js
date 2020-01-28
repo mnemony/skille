b0a064ebe9e63297e059e2e7910b4260c2692f57
Maksym <maxymprz@gmail.com>
2020-01-28 14:31:42 +0100

    pro cards

--- JS/linker.js
+++ JS/linker.js
@@ -6,3 +6,11 @@ function myGit() {
 function myLin() {
     window.open("https://www.linkedin.com/in/maksym-przewo%C5%BAny-b550b6156/", "_blank");
 };
+
+function bankLink() {
+    window.open("https://mnemony.github.io/supreme-bank/", "_blank");
+}
+
+function bankGit() {
+    window.open("https://github.com/mnemony/supreme-bank", "_blank");
+}
