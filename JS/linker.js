4cfe35bbfcfe68fff0367cbf4593587705d50844
Maksym <maxymprz@gmail.com>
2020-01-28 16:54:55 +0100

    alpha

--- JS/linker.js
+++ JS/linker.js
@@ -14,3 +14,28 @@ function bankLink() {
 function bankGit() {
     window.open("https://github.com/mnemony/supreme-bank", "_blank");
 }
+
+function prodLink() {
+    window.open("https://mnemony.github.io/production-panel/", "_blank");
+}
+
+function prodGit() {
+    window.open("https://github.com/mnemony/production-panel", "_blank");
+}
+
+
+function foodLink() {
+    window.open("https://mnemony.github.io/Nutritionconverter/", "_blank");
+}
+
+function foodGit() {
+    window.open("https://github.com/mnemony/Nutritionconverter", "_blank");
+}
+
+function rankLink() {
+    window.open("https://mnemony.github.io/githubRank/", "_blank");
+}
+
+function rankGit() {
+    window.open("https://github.com/mnemony/githubRank", "_blank");
+}
